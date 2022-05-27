f7dfae374f25f01cf384ab1a3902bcae06031d5c
CayeOficial <73038322+CayeOficial@users.noreply.github.com>
2022-05-27 23:47:11 +0200

    Update and rename lobbytools.java to LobbyTools.java

--- src/main/java/com/cayeoficial/LobbyTools.java
+++ src/main/java/com/cayeoficial/LobbyTools.java
@@ -3,6 +3,9 @@ package com.cayeoficial;
 import com.cayeoficial.commands.ReloadCommand;
 import com.cayeoficial.commands.SetSpawnCommand;
 import com.cayeoficial.listeners.*;
+import org.bukkit.Bukkit;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -11,10 +14,12 @@ import java.util.Arrays;
 import java.util.Objects;
 
 
-public final class lobbytools extends JavaPlugin {
+public final class LobbyTools extends JavaPlugin implements Listener {
 
+    private Plugin plugin;
     @Override
     public void onEnable() {
+        plugin = this;
         getLogger().info("§aInitializing [Lobby-Tools]");
         getLogger().info("§aInitializing Lobby-Tools modules...");
         try {
@@ -26,6 +31,7 @@ public final class lobbytools extends JavaPlugin {
         getLogger().info("§cHere's the error StackTrace, please include it when contacting the owner of the plugin.");
         exception.printStackTrace();
         }
+
     }
 
     @Override
@@ -45,7 +51,20 @@ public final class lobbytools extends JavaPlugin {
                 new playerChat(this),
                 new playerInteract(this)
         ).forEach(event -> pluginManager.registerEvents(event, this));
-                
+        Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, () -> {
+        if(checkForIncompatibilities()) {
+        Arrays.asList(
+                "##### WARNING #####",
+                "",
+                "[!] DeluxeHub is installed on your server, you",
+                "can't actually use both without issues.",
+                "",
+                "Please remove DeluxeHub from your plugin list",
+                "",
+                "##### WARNING #####"
+        ).forEach(string -> getLogger().warning(string));
+            }
+        }, 0L);
     }
     public void commands() {
         Objects.requireNonNull(this.getCommand("setspawn")).setExecutor(new SetSpawnCommand(this));
@@ -59,4 +78,13 @@ public final class lobbytools extends JavaPlugin {
             saveDefaultConfig();
         }
     }
+
+    public Plugin getPlugin() {
+        return plugin;
+    }
+
+    private boolean checkForIncompatibilities() {
+        return Bukkit.getPluginManager().isPluginEnabled("DeluxeHub");
+    }
+
  }

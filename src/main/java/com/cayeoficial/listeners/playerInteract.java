59b57bef54cb53bbe71e20106659ab4eeb56ad5e
CayeOficial <73038322+CayeOficial@users.noreply.github.com>
2022-05-27 23:52:55 +0200

    Rename lobbytools -> LobbyTools

--- src/main/java/com/cayeoficial/listeners/playerInteract.java
+++ src/main/java/com/cayeoficial/listeners/playerInteract.java
@@ -1,7 +1,7 @@
 package com.cayeoficial.listeners;
 
 import com.cayeoficial.helpers.ConfigHelper;
-import com.cayeoficial.lobbytools;
+import com.cayeoficial.LobbyTools;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -10,8 +10,8 @@ import org.bukkit.event.player.PlayerInteractEvent;
 import java.util.Objects;
 
 public class playerInteract implements Listener {
-    private final lobbytools plugin;
-    public playerInteract(lobbytools plugin) {
+    private final LobbyTools plugin;
+    public playerInteract(LobbyTools plugin) {
         this.plugin = plugin;
     }
 

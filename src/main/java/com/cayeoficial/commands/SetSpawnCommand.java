b3c55af31fbb2616fa2f01ea8fa11cf826f93352
CayeOficial <73038322+CayeOficial@users.noreply.github.com>
2022-05-28 23:57:00 +0200

    Added comments and changed setSpawn method

--- src/main/java/com/cayeoficial/commands/SetSpawnCommand.java
+++ src/main/java/com/cayeoficial/commands/SetSpawnCommand.java
@@ -1,42 +1,40 @@
 package com.cayeoficial.commands;
 
+import com.cayeoficial.LobbyTools;
 import com.cayeoficial.helpers.ConfigHelper;
 import com.cayeoficial.helpers.MessageHelper;
-import com.cayeoficial.LobbyTools;
+import com.cayeoficial.helpers.SpawnHelper;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 public class SetSpawnCommand implements CommandExecutor {
     private final LobbyTools plugin;
-    public SetSpawnCommand(LobbyTools plugin) {
-        this.plugin = plugin;
-    }
+    public SetSpawnCommand(LobbyTools plugin) { this.plugin = plugin; }
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
-        FileConfiguration config = plugin.getConfig();
-
+        
+        /*
+        * Check if the command is executed via console
+        */
         if(!(sender instanceof Player)) {
             Bukkit.getConsoleSender().sendMessage(ConfigHelper.getString("Messages.cannot-execute-by-console"));
             return false;
         } else {
-            Player p = (Player) sender;
-            if(p.hasPermission("lobbytools.admin")) {
-                config.set("Spawn.world", p.getLocation().getWorld().getName());
-                config.set("Spawn.x", p.getLocation().getX());
-                config.set("Spawn.y", p.getLocation().getY());
-                config.set("Spawn.z", p.getLocation().getZ());
-                config.set("Spawn.yaw", p.getLocation().getYaw());
-                config.set("Spawn.pitch", p.getLocation().getPitch());
+            /*
+            * Set the spawn if it's a player and have permissions
+            */
+            Player player = (Player) sender;
+            if(player.hasPermission("LobbyTools.admin")) {
+                SpawnHelper.setSpawn(player);
                 plugin.saveConfig();
-                MessageHelper.sendMessage(p, "Spawn.spawn-set-message");
+                MessageHelper.sendMessage(player, "Spawn.spawn-set-message");
             } else {
-                MessageHelper.sendMessage(p, "Messages.no-permission-error");
+                MessageHelper.sendMessage(player, "Messages.no-permission-error");
             }
 
         }

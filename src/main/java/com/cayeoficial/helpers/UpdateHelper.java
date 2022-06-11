b0d71805af4fabfd6189776b2c9d2d038035288a
CayeOficial <73038322+CayeOficial@users.noreply.github.com>
2022-06-12 00:52:33 +0200

    Create UpdateHelper.java

---
+++ src/main/java/com/cayeoficial/helpers/UpdateHelper.java
@@ -0,0 +1,37 @@
+package com.cayeoficial.helpers;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Scanner;
+import java.util.function.Consumer;
+/*
+* This public class can be found at https://www.spigotmc.org/wiki/creating-an-update-checker-that-checks-for-updates
+* I didn't made it!
+*/
+
+public class UpdateHelper {
+
+    private final JavaPlugin plugin;
+    private final int resourceId;
+
+    public UpdateHelper(JavaPlugin plugin, int resourceId) {
+        this.plugin = plugin;
+        this.resourceId = resourceId;
+    }
+
+    public void getVersion(final Consumer<String> consumer) {
+        Bukkit.getScheduler().runTaskAsynchronously(this.plugin, () -> {
+            try (InputStream inputStream = new URL("https://api.spigotmc.org/legacy/update.php?resource=" + this.resourceId).openStream(); Scanner scanner = new Scanner(inputStream)) {
+                if (scanner.hasNext()) {
+                    consumer.accept(scanner.next());
+                }
+            } catch (IOException exception) {
+                plugin.getLogger().info("Unable to check for updates: " + exception.getMessage());
+            }
+        });
+    }
+}
